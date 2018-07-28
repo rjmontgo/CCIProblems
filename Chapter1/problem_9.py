34a393c182ca66e1a46c086c4480cb96f4d1ac3a
Robert Montgomery <robm4958@gmail.com>
2018-07-28 15:15:49 -0400

    Finished Problem 9 with tests

--- Chapter1/problem_9.py
+++ Chapter1/problem_9.py
@@ -6,7 +6,8 @@
 # rotation of "erbottlewat").
 #
 def isRotation(s1, s2):
-
+    concat = s2 + s2
+    return s1 in concat
 
 # Parameters -> (String: s1, String: s2) -> Can I trust correct input? Yes
 # Return value -> True if s2 is a rotation of s1; else False
@@ -38,3 +39,20 @@ def isRotation(s1, s2):
 # V7
 # IN - ("testing", "tesitng")
 # OUT - False
+
+# Patterns
+# - Perhaps better to think of it as swapping parts of the string rather than
+# rotating (i.e bottlewater is waterbottle when swapping 'bottle' and 'water'
+# - Adding bottlewater + bottlewater we have bottlewaterbottlewater
+#   - The original string is a substring
+
+#
+# Solution / complexity where n is the length of s2
+# 1. Concatenate s2 and check if it has s1 as a substring
+#    / O(n) - time due to substring check and concatenation.
+#    / O(n) - space due to having to make a new string to hold the result of
+#             concatenation.
+#
+# 2. Rotate s2 by one char until we reach the original string or we equal s1
+#    / O(n^2) - time because we must loop the rotate operation n times and the
+#               rotate operation states we
--- Chapter1/test_problem_9.py
+++ Chapter1/test_problem_9.py
@@ -1,5 +1,30 @@
 import unittest
 
-from poblem_8 import isRotation
+from problem_9 import isRotation
 
 class TestIsRotation(unittest.TestCase):
+
+    def test_one_char(self):
+        self.assertEqual(isRotation("a", "a"), True)
+
+    def test_two_chars(self):
+        self.assertEqual(isRotation("ab", "ab"), True)
+
+    def test_watermelon(self):
+        self.assertEqual(isRotation("watermelon", "lonwaterme"), True)
+
+    def test_reduce(self):
+        self.assertEqual(isRotation("reduce", "ucered"), True)
+
+    def test_waterbottle(self):
+        self.assertEqual(isRotation("waterbottle", "erbottlewat"), True)
+
+    def test_testin_fail(self):
+        self.assertEqual(isRotation("testing", "testin"), False)
+
+    def test_tesiting_fail(self):
+        self.assertEqual(isRotation("testing", "tesiting"), False)
+
+
+if __name__ is '__main__':
+    unittest.main()
